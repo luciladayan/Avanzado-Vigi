a04c1775d540e28ef398b0dae9fdfe5d5441a483
jazminniew <jazminniew@gmail.com>
2024-09-18 14:38:12 -0300

    auth controller terminado igual chequear

--- burgertic/controllers/auth.controller.js
+++ burgertic/controllers/auth.controller.js
@@ -1,26 +1,10 @@
 import UsuariosService from "../services/usuarios.service.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+import usuariosService from "../services/usuarios.service.js";
 
 const register = async (req, res) => {
-    // --------------- COMPLETAR ---------------
-    /*
-
-        Recordar que para cumplir con toda la funcionalidad deben:
-
-            1. Verificar que el body de la request tenga el campo usuario
-            2. Verificar que el campo usuario tenga los campos nombre, apellido, email y password
-            3. Verificar que no exista un usuario con el mismo email (utilizando el servicio de usuario)
-            4. Devolver un mensaje de error si algo falló hasta el momento (status 400)
-            5. Hashear la contraseña antes de guardarla en la base de datos
-            6. Guardar el usuario en la base de datos (utilizando el servicio de usuario)
-            7. Devolver un mensaje de éxito si todo salió bien (status 201)
-            8. Devolver un mensaje de error si algo falló guardando al usuario (status 500)
-        
-    */
-
 try {
-
     /*1*/
     const {usuario} = req.body;
     if(!usuario) return 
@@ -35,28 +19,19 @@ try {
     if (usuario)
     return res.status(404).json({ message: "Usuario ya existente" });
     
-
     /*5*/
     usuario.password = await bcrypt.hash(usuario.password, 10)
-    
-/*6  */
-const usuarioInsert = await UsuariosService.createUsuario(usuario)
-if(!usuarioInsert) return  res.status(500).json({ message: error.message });
-res.status(200).json({ message: "exito" });
 
+    /*6  */
+    const usuarioInsert = await UsuariosService.createUsuario(usuario)
+    res.status(200).json({ message: "exito" });
+    if(!usuarioInsert) return  res.status(500).json({ message: error.message });
 
 } catch (error) {
     /*4*/
     res.status(500).json({ message: error.message });
 }
-
-
-
 }
-
-
-
-
 
 const login = async (req, res) => {
     // --------------- COMPLETAR ---------------
@@ -72,8 +47,35 @@ const login = async (req, res) => {
             6. Crear un token con el id del usuario y firmarlo con la clave secreta (utilizando la librería jsonwebtoken)
             7. Devolver un json con el usuario y el token (status 200)
             8. Devolver un mensaje de error si algo falló (status 500)
-        
     */
-};
+    
+    try {
+        /*1*/
+        const {email, password} = req.body;
+        if(!email || !password) return res.status(400).json({ message: "Faltan campos" });
+        
+        /*2*/
+        const usuario = await UsuariosService.getUsuarioByEmail(email);
+
+        /*3*/
+        if (!usuario) return res.status(400).json({ message: "Usuario no encontrado" });
+    
+        /*4*/
+        const contraseñaCorrecta = bcrypt.compare(password, usuario.password);
+        if (!contraseñaCorrecta) return res.status(400).json({ message: "La contraseña no es correcta"})
+            
+        /*5*/
+        const token =  jwt.sign({id: usuario.id}, SECRET, {expiresIn: "30m"});
+
+        res.send(token)
+        }   
+
+        catch (error) {
+            res.status(500).json({ message: error.message });
+        }
+    }
+
+
+
 
 export default { register, login };
