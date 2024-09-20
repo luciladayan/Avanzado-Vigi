9482c96fbb1aea1286334ef4839f5414f25dfd4d
jazminniew <jazminniew@gmail.com>
2024-09-20 16:58:46 -0300

    error

--- burgertic/middlewares/auth.middleware.js
+++ burgertic/middlewares/auth.middleware.js
@@ -6,7 +6,7 @@ export const verifyToken = async (req, res, next) => {
 
     try {
         if (!req.headers.authorization) return res.status(400).send("No hay headers de autorizacion");
-        const { token } = req.headers.authorization.split(" ")[1]
+        const token = req.headers.authorization.split(" ")[1]
         if (!token) return res.status(400).send("Esta en el formato incorrecto");
         const val = await jwt.verify(token, process.env.SECRET);
         if (!val) return res.status(403).send("El token no es valido");
@@ -16,7 +16,7 @@ export const verifyToken = async (req, res, next) => {
         next();
     }
     catch (error) {
-        res.status(401).send("Hubo un problema");
+        res.status(401).send({ message: error.message });
     }
 
 };
